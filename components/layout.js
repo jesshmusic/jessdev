6e06c4b27b4ac51ad4d1436e9e732bbfe2fa4db9
jesshmusic <jhendricks@existentialmusic.com>
2020-10-11 17:30:30 -0400

    Favicons added

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,10 @@ export default function Layout({ children, home, title }) {
   return (
     <div className={styles.content} style={{backgroundImage: 'url("/images/logo.svg")'}}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
