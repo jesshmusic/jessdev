d94936492b699729ecd37f135c08347b8a6bbfb4
jesshmusic <jhendricks@existentialmusic.com>
2020-10-11 15:48:06 -0400

    Fixed an import for the DateComponent.js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
-import DateComponent from '../../components/date'
+import DateComponent from '../../components/DateComponent'
 
 export default function Post({ postData }) {
   return (
