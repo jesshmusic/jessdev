82c869cee0b0f075dc5616624c99a9514c443e67
jesshmusic <jhendricks@existentialmusic.com>
2020-10-11 16:19:14 -0400

    Logo Background image

--- components/layout.js
+++ components/layout.js
@@ -5,19 +5,17 @@ import Link from 'next/link';
 
 export default function Layout({ children, home, title }) {
   return (
-    <div className={styles.content}>
+    <div className={styles.content} style={{backgroundImage: 'url("/images/logo.svg")'}}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
         />
-        {/*<meta*/}
-        {/*  property="og:image"*/}
-        {/*  content={`https://og-image.now.sh/${encodeURI(*/}
-        {/*    siteTitle*/}
-        {/*  )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}*/}
-        {/*/>*/}
+        <meta
+          property="og:image"
+          content={`/images/logo.svg`}
+        />
         <meta name="og:title" content={title} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
