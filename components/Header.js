076f12c257ce5db009d14920e6d952cc87dc6f21
jesshmusic <jhendricks@existentialmusic.com>
2020-11-03 12:23:06 -0500

    Minor Redesign: extracted the Header to a component

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import styles from './Header.module.scss'
+import Link from 'next/link'
+import Logo from './Logo'
+import { PropTypes } from 'prop-types'
+
+const Header = ({ home }) => (
+  <header className={styles.header}>
+    {home ? (
+      <h1 id={'siteHeadingText'}>
+        Jess Hendricks <small>Front End and Full Stack Developer</small>
+      </h1>
+    ) : (
+      <h1>
+        <Link href="/">
+          <a id={'siteHeadingText'}>
+            Jess Hendricks <small>Front End and Full Stack Developer</small>
+          </a>
+        </Link>
+      </h1>
+    )}
+    <Logo />
+  </header>
+)
+
+Header.propTypes = {
+  home: PropTypes.bool
+}
+
+export default Header
