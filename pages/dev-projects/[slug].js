3ec8bd09f00cdc28ecf097801391a0fb83ef6db8
jesshmusic <jhendricks@existentialmusic.com>
2020-11-04 10:23:40 -0500

    Minor Redesign: Project Page

--- pages/dev-projects/[slug].js
+++ pages/dev-projects/[slug].js
@@ -4,6 +4,7 @@ import ReactMarkdown from 'react-markdown'
 import styles from './project.module.scss'
 import { Carousel } from 'react-responsive-carousel'
 import { PropTypes } from 'prop-types'
+import TechTease from '../../components/TechTease'
 
 export default function Post ({ postData }) {
   const buttonText = postData.link && postData.link.includes('github') ? 'View on Github' : 'View App';
@@ -11,17 +12,29 @@ export default function Post ({ postData }) {
   return (
     <Layout>
       <h2 className={styles.pageTitle}>{ postData.title }</h2>
-      <div className={styles.contentRow}>
+      <article className={styles.mainContent}>
         <div className={styles.content}>
           <ReactMarkdown source={postData.content} />
         </div>
         <div className={styles.thumbnail}>
           <img src={postData.thumbnail.url} alt={postData.thumbnail.alt}/>
         </div>
-      </div>
-      <div className={styles.contentRow}>
-
-      </div>
+      </article>
+      {postData.tech && postData.tech.length > 0 ? (
+        <section className={styles.contentRow} id={'techSection'}>
+          <h3 className={styles.heading}>Technology</h3>
+          <div className={styles.cardGrid}>
+            {postData.tech.map((tech) => {
+              return <TechTease
+                logo={ tech.icon }
+                title={ tech.title }
+                description={ tech.description }
+                key={ tech.id }
+                scrollTrigger={ '#techSection' }/>
+            })}
+          </div>
+        </section>
+      ) : null}
       <div className={styles.linkRow}>
         <a href={postData.link}
           className={styles.githubLink}
@@ -32,7 +45,7 @@ export default function Post ({ postData }) {
       </div>
       {postData.gallery && postData.gallery.length > 0 ? (
         <div className={styles.gallery}>
-          <h2>Screenshots</h2>
+          <h3 className={styles.heading}>Screenshots</h3>
           <Carousel autoPlay={true} width={'50vh'}>
             {postData.gallery.map(image => (
               <div key={image.id}>
@@ -51,6 +64,14 @@ Post.propTypes = {
     content: PropTypes.string.isRequired,
     gallery: PropTypes.array,
     link: PropTypes.string.isRequired,
+    tech: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        icon: PropTypes.object,
+        title: PropTypes.string,
+        description: PropTypes.string
+      })
+    ),
     title: PropTypes.string.isRequired
   })
 }
